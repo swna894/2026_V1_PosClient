fd4b0a9a58a915b77803643e958cf84befc55ef9
Sangwoon Na <swna8934@gmail.com>
2026-05-03 00:22:50 +0900

    refactor: PosView.fxml, pos_main.css
    - 크래스 정리 및 크래스명 개선

--- src/main/java/com/swna/javafx/controller/pos/PosViewController.java
+++ src/main/java/com/swna/javafx/controller/pos/PosViewController.java
@@ -22,7 +22,6 @@ import javafx.animation.Timeline;
 import javafx.application.Platform;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
-import javafx.scene.Node;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.scene.control.Alert;
